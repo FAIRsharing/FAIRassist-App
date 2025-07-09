acbd6ebcd64833900727dca8bbb20d20a7f28e39
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2025-07-09 12:23:51 +0100

    fixing test case by mocking d3js

--- src/components/Registry/__tests__/CollapseTreeGraph.spec.js
+++ src/components/Registry/__tests__/CollapseTreeGraph.spec.js
@@ -1,6 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import { createVuetify } from "vuetify";
-import { beforeEach, describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import CollapseTreeGraph from "../CollapseTreeGraph";
 import axios from "axios";
 import sinon from "sinon";
@@ -9,6 +9,7 @@ import d3GraphData from "./data/d3GraphData.json";
 const vuetify = createVuetify();
 
 describe("CollapseTreeGraph.vue", function () {
+  vi.mock("d3", () => ({}));
   let wrapper;
   let getStub = sinon.stub(axios, "get");
   getStub.withArgs(sinon.match.any).returns(d3GraphData);

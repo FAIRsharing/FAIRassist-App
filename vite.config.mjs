97b4fccec80e3ae793f8074f6a6b7847ca66dd49
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2025-07-17 16:36:28 +0100

    adding json-to-graphql-query in vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,73 +1,77 @@
-import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "url";
+import {defineConfig} from "vite";
+import {fileURLToPath, URL} from "url";
 import vue from "@vitejs/plugin-vue";
-import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import vuetify, {transformAssetUrls} from "vite-plugin-vuetify";
 import dns from "node:dns";
 import eslintPlugin from "vite-plugin-eslint";
 import path from 'path'
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
 import compress from 'vite-plugin-compress'
+
 dns.setDefaultResultOrder("verbatim");
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "./",
-  server:{
-    host: true,
-    open: true,
-    port: 8084
-  },
-  preview: {
-    host: true,
-    open: true,
-    port: 8085,
-  },
-  test: {
-    environment: "jsdom",
-    globals: true,
-  },
-  plugins: [
-    vue({ template: { transformAssetUrls } }),
-    vuetify({
-      autoImport: true,
-    }),
-    eslintPlugin,
-    nodePolyfills(),
-    compress,
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "source-map-js": "source-map"
+    base: "./",
+    server: {
+        host: true,
+        open: true,
+        port: 8084
     },
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue", ".svg"],
-  },
-  optimizeDeps: {
-    include: ["vuetify"],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/styles/index.scss";`,
-        api: "modern-compiler", // or "modern"
-      },
+    preview: {
+        host: true,
+        open: true,
+        port: 8085,
     },
-  },
-  build: {
-    // outDir: 'dist',
-    assetsDir: 'assets',
-    minify: 'esbuild',
-    target: 'es2015',
-    cssCodeSplit: true,
-    chunkSizeWarningLimit: 1250,
-    rollupOptions: {
-      input:path.resolve(__dirname, "index.html"),
-      output: {
-        dir: "dist",
-        format: "es"
-      },
+    test: {
+        environment: "jsdom",
+        globals: true,
+    },
+    plugins: [
+        vue({template: {transformAssetUrls}}),
+        vuetify({
+            autoImport: true,
+        }),
+        eslintPlugin,
+        nodePolyfills(),
+        compress,
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            "source-map-js": "source-map"
+        },
+        extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue", ".svg"],
+    },
+    optimizeDeps: {
+        include: ["vuetify"],
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "@/styles/index.scss";`,
+                api: "modern-compiler", // or "modern"
+            },
+        },
+    },
+    build: {
+        // outDir: 'dist',
+        assetsDir: 'assets',
+        minify: 'esbuild',
+        target: 'es2015',
+        cssCodeSplit: true,
+        chunkSizeWarningLimit: 1250,
+        rollupOptions: {
+            input: path.resolve(__dirname, "index.html"),
+            output: {
+                dir: "dist",
+                format: "es"
+            },
+            external: [
+                "json-to-graphql-query",
+            ],
+        }
     }
-  }
 });

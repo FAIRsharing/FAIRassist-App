4157cebb97d9bf007058c149aa583b1901cd8a3e
prakhyatox <prakhyat.gailani@eng.ox.ac.uk>
2025-08-07 14:47:18 +0100

    adding test cases in ApplyFilterButton.spec.js

--- src/components/Registry/__tests__/ApplyFilterButton.spec.js
+++ src/components/Registry/__tests__/ApplyFilterButton.spec.js
@@ -3,6 +3,7 @@ import { createVuetify } from "vuetify";
 import { beforeEach, describe, expect, it } from "vitest";
 import ApplyFilterButton from "../ApplyFilterButton.vue";
 import { createPinia, setActivePinia } from "pinia";
+import { useAdvancedSearchStore } from "@/stores/advancedSearch.js";
 
 const vuetify = createVuetify();
 
@@ -20,6 +21,14 @@ describe("ApplyFilterButton.vue", function () {
   });
 
   it("can be instantiated", () => {
+    wrapper.vm.$options.watch.getFairassistID.call(wrapper.vm, 12);
     expect(wrapper.vm.$options.name).toMatch("ApplyFilterButton");
+  });
+
+  it("check fetchAdvancedSearchResults called on click of 'Apply Filter' button", () => {
+    const store = useAdvancedSearchStore();
+    const applyFilterButton = wrapper.get("[data-testid='applyFilter']");
+    applyFilterButton.trigger("click");
+    expect(store.fetchAdvancedSearchResults()).toHaveBeenCalled;
   });
 });
